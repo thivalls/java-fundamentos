ba5a5ab7f39e0bc51cef965b3436c60daf2a6c70
valls <olavalls@gmail.com>
2021-08-31 23:57:52 -0300

    Aplicando a interface funcional nativa BinaryOperator para outras operacoes

--- src/main/java/ApplicationLambda.java
+++ src/main/java/ApplicationLambda.java
@@ -5,7 +5,10 @@ import java.util.function.BinaryOperator;
 public class ApplicationLambda {
     public static void main(String[] args) {
         // Usando uma interface nativa do Java
-        BinaryOperator<Double> bin = (value1, value2) -> value1 + value2;
+        BinaryOperator<Double> binSum = (value1, value2) -> value1 + value2;
+        BinaryOperator<Double> binSub = (value1, value2) -> value1 - value2;
+        BinaryOperator<Double> binMulti = (value1, value2) -> value1 * value2;
+        BinaryOperator<Double> binDiv = (value1, value2) -> value1 / value2;
 
         Calculo div = (value1, value2) -> value1 / value2;
         Calculo multi = (value1, value2) -> value1 * value2;
@@ -16,7 +19,10 @@ public class ApplicationLambda {
         System.out.println("Multiplicando -> " + multi.executar(33, 3));
         System.out.println("Somando -> " + sum.executar(33, 3));
         System.out.println("Subtraindo -> " + sub.executar(33, 3));
-        System.out.println("Utilizando BinaryOperator -> " + bin.apply(33.0, 3.0));
+        System.out.println("Utilizando BinaryOperator SOMA-> " + binSum.apply(33.0, 3.0));
+        System.out.println("Utilizando BinaryOperator SUBTRACAO-> " + binSub.apply(33.0, 3.0));
+        System.out.println("Utilizando BinaryOperator MULTIPLICACAO-> " + binMulti.apply(33.0, 3.0));
+        System.out.println("Utilizando BinaryOperator DIVISAO-> " + binDiv.apply(33.0, 3.0));
 
         /*
          * mostrando que podemos criar métodos default e estáticos para trabalhar com intefaces
