391d923afdf34b87aff688203ba25f2b979720ed
valls <olavalls@gmail.com>
2021-09-04 00:27:36 -0300

    Aprendendo interface Function e como podemos encadear varias funcoes

---
+++ src/main/Funcao.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.function.Function;
+
+public class Funcao {
+    public static void main(String[] args) {
+        System.out.println("Utilizando interface FUNCTION");
+        System.out.println("+++++++++++++++++++++++++++++");
+
+        Function<Integer, String> checkEvenOdd = integer -> integer % 2 == 0 ? "par" : "ímpar";
+        Function<String, String> printResult = text -> "O resultado é " + text;
+        Function<String, String> sufixoEmpolgado = whatEverString -> whatEverString + "!!!";
+
+        Integer inteiro = 101;
+
+        System.out.println("UTILIZANDO MÉTODO ENDTHEN PARA ENCADEAR FUNÇÕES");
+        System.out.println(checkEvenOdd.andThen(printResult).andThen(sufixoEmpolgado).apply(inteiro));
+
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("ENCADEANDO FUNÇÕES MANUALMENTE");
+        System.out.println(sufixoEmpolgado.apply(printResult.apply(checkEvenOdd.apply(inteiro))));
+
+    }
+}
